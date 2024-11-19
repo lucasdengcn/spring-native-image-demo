6d67630842245403be140b44b79f4e5c49839872
yamingdeng <yamingdeng@outlook.com>
2024-11-19 14:37:30 +0800

    add caffeine reflection-config

--- src/main/kotlin/com/example/demo/nativeimage/CustomRuntimeHints.kt
+++ src/main/kotlin/com/example/demo/nativeimage/CustomRuntimeHints.kt
@@ -1,10 +1,11 @@
 package com.example.demo.nativeimage
 
-import com.example.demo.service.OrderService
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.aot.hint.MemberCategory
 import org.springframework.aot.hint.RuntimeHints
 import org.springframework.aot.hint.RuntimeHintsRegistrar
+import org.springframework.aot.hint.TypeReference
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.ImportRuntimeHints
 
@@ -18,5 +19,12 @@ class CustomRuntimeHints : RuntimeHintsRegistrar {
 
     override fun registerHints(hints: RuntimeHints, classLoader: ClassLoader?) {
         logger.info("registerHints")
+        val clazz = TypeReference.of("com.github.benmanes.caffeine.cache.SSLMSA")
+        hints.reflection().registerType(clazz) {
+            it.withMembers(MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS)
+            it.withMembers(MemberCategory.INVOKE_PUBLIC_METHODS)
+            it.withMembers(MemberCategory.DECLARED_FIELDS)
+            it.withMembers(MemberCategory.PUBLIC_CLASSES)
+        }
     }
 }
